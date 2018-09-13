a728df5bc46eb3a683b38d5f8adf46bb87c1060d
Unknown <noah.steele.2021@student.lhschools.org>
2018-09-13 08:53:24 -0400

    Worked on project

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class NumberGuessingGame {
 
     public static void main(String[] args){
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        int pickedNumber, guessedNumber;
+
+        pickedNumber = (int)(Math.random()*100);
+
+        System.out.println(pickedNumber);
+
+        System.out.println("Pick a number between 1 and 100");
+        guessedNumber = keyboard.nextInt();
+
         
+
+        if (guessedNumber>pickedNumber){System.out.println("Too high! Try again.");}
+        else if (guessedNumber<pickedNumber){System.out.println("Too low! Try again.");}
+
+        if (guessedNumber==pickedNumber){System.out.println("You win!");}
 
 
 
