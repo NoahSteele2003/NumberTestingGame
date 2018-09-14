fc15b6c328f8922dffb67017850f7cd8000b14d9
Unknown <noah.steele.2021@student.lhschools.org>
2018-09-14 07:38:36 -0400

    Finished project

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -7,23 +7,21 @@ public class NumberGuessingGame {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
-        int pickedNumber, guessedNumber;
+        int pickedNumber, guessedNumber, tries;
+        tries = 0;
 
         pickedNumber = (int)(Math.random()*100);
 
-        System.out.println(pickedNumber);
+        /*Temporary*/ //System.out.println(pickedNumber);
 
         System.out.println("Pick a number between 1 and 100");
         guessedNumber = keyboard.nextInt();
 
-        
+        while (guessedNumber != pickedNumber){
+            if (guessedNumber>pickedNumber){System.out.println("Too high! Try again."); tries+=1;guessedNumber = keyboard.nextInt();}
+            else if (guessedNumber<pickedNumber){System.out.println("Too low! Try again."); tries+=1;guessedNumber = keyboard.nextInt();}}
 
-        if (guessedNumber>pickedNumber){System.out.println("Too high! Try again.");}
-        else if (guessedNumber<pickedNumber){System.out.println("Too low! Try again.");}
-
-        if (guessedNumber==pickedNumber){System.out.println("You win!");}
-        
-        //make sure to use while statements
+            if (guessedNumber==pickedNumber){tries+=1;System.out.println("You win! And it only took you "+tries+" tries!");}
 
 
 
