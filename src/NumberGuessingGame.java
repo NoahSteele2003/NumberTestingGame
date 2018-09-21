d617448b13c403c7533ee3b39056e1e68737ec7d
Unknown <noah.steele.2021@student.lhschools.org>
2018-09-21 08:04:20 -0400

    Finally got back to the point where I was before I messed up

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -29,6 +29,33 @@ public class NumberGuessingGame {
             tries+=1;
             System.out.println("You Win! And it only took you "+tries+" tries.");}
 
+        System.out.println("Would you like to play again?");
+        response = keyboard.next();
+
+        while(response.equals("yes")){
+            pickedNumber = (int)(Math.random()*100);
+            tries = 0;
+            System.out.println("Pick a number between 1 and 100.");
+            guessedNumber=keyboard.nextInt();
+            while (guessedNumber!=pickedNumber){
+            if (guessedNumber < pickedNumber){System.out.println("Too low! Try again");
+                guessedNumber = keyboard.nextInt(); tries+=1;}
+            else if(guessedNumber>pickedNumber){System.out.println("Too high! Try again");
+                guessedNumber = keyboard.nextInt(); tries+=1;}}
+
+            if (guessedNumber==pickedNumber){
+                tries+=1;
+                System.out.println("You Win! And it only took you "+tries+" tries.");}
+
+            System.out.println("Would you like to play again?");
+            response = keyboard.next();}
+
+
+
+        if(response.equals("no")){
+            System.out.println("Ok then.");
+        }
+
 
 
 
