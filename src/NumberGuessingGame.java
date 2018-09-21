2a2a241450fcbc3fb60d99c3f7dd7d20933f9891
Unknown <noah.steele.2021@student.lhschools.org>
2018-09-21 07:38:32 -0400

    reset

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -19,7 +19,7 @@ public class NumberGuessingGame {
         guessedNumber = keyboard.nextInt();
 
 
-
+        //first play
             while (guessedNumber!=pickedNumber){
                 if (guessedNumber < pickedNumber){System.out.println("Too low! Try again");
                     guessedNumber = keyboard.nextInt(); tries+=1;}
@@ -28,19 +28,37 @@ public class NumberGuessingGame {
 
             if (guessedNumber==pickedNumber){
                 tries+=1;
-                System.out.println("You Win! And it only took you "+tries+" tries.");}
+                System.out.println("You Win! And it only took you "+tries+" tries.");
+                System.out.println("Would you like to play again?");}
+
+                response = keyboard.next();
 
 
+            //next plays
+
+            while(response.equals("yes")){
+                response = "complete";
+                tries = 0;
+                pickedNumber = (int)(Math.random()*100);
+                //System.out.println(pickedNumber);
+                System.out.println("Pick a number between 1 and 100");
+                guessedNumber = keyboard.nextInt();
+                while (guessedNumber!=pickedNumber){
+                    if (guessedNumber < pickedNumber){System.out.println("Too low! Try again");
+                        guessedNumber = keyboard.nextInt(); tries+=1;}
+                    else if(guessedNumber>pickedNumber){System.out.println("Too high! Try again");
+                        guessedNumber = keyboard.nextInt(); tries+=1;}}
+
+                if (guessedNumber==pickedNumber){
+                    tries+=1;
+                    System.out.println("You Win! And it only took you "+tries+" tries.");
+                    System.out.println("Would you like to play again?");}
+                    response = keyboard.next();}
 
 
+                    //Not playing again
 
-
-
-
-
-
-
-
+                    if(response.equals("no")){System.out.println("Ok then.");}
 
 
 
